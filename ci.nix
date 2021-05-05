ddad8fe511b54cbc06b6c975cec64e3e49c9e5f1
Pavel Borzenkov <pavel@borzenkov.net>
2021-05-05 19:08:04 +0300

    osccopy: init at 0.1.0

--- ci.nix
+++ ci.nix
@@ -1,15 +1,4 @@
-# This file provides all the buildable and cacheable packages and
-# package outputs in your package set. These are what gets built by CI,
-# so if you correctly mark packages as
-#
-# - broken (using `meta.broken`),
-# - unfree (using `meta.license.free`), and
-# - locally built (using `preferLocalBuild`)
-#
-# then your CI will be able to build and cache only those packages for
-# which this is possible.
-
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import <nixpkgs> {} }:
 
 with builtins;
 let
@@ -28,9 +17,9 @@ let
       f = p:
         if shouldRecurseForDerivations p then flattenPkgs p
         else if isDerivation p then [ p ]
-        else [ ];
+        else [];
     in
-    concatMap f (attrValues s);
+      concatMap f (attrValues s);
 
   outputsOf = p: map (o: p.${o}) p.outputs;
 
@@ -38,10 +27,16 @@ let
 
   nurPkgs =
     flattenPkgs
-      (listToAttrs
-        (map (n: nameValuePair n nurAttrs.${n})
-          (filter (n: !isReserved n)
-            (attrNames nurAttrs))));
+      (
+        listToAttrs
+          (
+            map (n: nameValuePair n nurAttrs.${n})
+              (
+                filter (n: !isReserved n)
+                  (attrNames nurAttrs)
+              )
+          )
+      );
 
 in
 rec {
