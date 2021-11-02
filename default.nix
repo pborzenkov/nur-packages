24f61c67f799c9c108e3304e9f6e00236737810f
Pavel Borzenkov <pavel@borzenkov.net>
2021-11-02 17:09:48 +0100

    transmission-exporter: init at 0.1.2

--- default.nix
+++ default.nix
@@ -6,5 +6,6 @@ rec {
   osccopy = pkgs.callPackage ./pkgs/osccopy { };
   tg-bot-skyeng = pkgs.callPackage ./pkgs/tg-bot-skyeng { };
   tg-bot-transmission = pkgs.callPackage ./pkgs/tg-bot-transmission { };
+  transmission-exporter = pkgs.callPackage ./pkgs/transmission-exporter { };
   vlmcsd = pkgs.callPackage ./pkgs/vlmcsd { };
 }
